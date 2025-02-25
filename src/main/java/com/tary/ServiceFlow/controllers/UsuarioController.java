c50867303d13c4cb33c7dcb2ef959ceb7336a34b
Tary Junior <111458237+TN-Junior@users.noreply.github.com>
2025-02-25 16:17:43 -0300

    Update UsuarioController.java

--- src/main/java/com/tary/ServiceFlow/controllers/UsuarioController.java
+++ src/main/java/com/tary/ServiceFlow/controllers/UsuarioController.java
@@ -17,14 +17,14 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
-    // Criar um novo usuário
+
     @PostMapping
     public ResponseEntity<Usuario> criarUsuario(@Valid @RequestBody Usuario usuario) {
         Usuario novoUsuario = usuarioService.criarUsuario(usuario);
         return ResponseEntity.ok(novoUsuario);
     }
 
-    // Buscar usuário pelo ID
+
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
         return usuarioService.buscarPorId(id)
@@ -32,13 +32,13 @@ public class UsuarioController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // Listar todos os usuários
+
     @GetMapping
     public ResponseEntity<List<Usuario>> listarUsuarios() {
         return ResponseEntity.ok(usuarioService.listarUsuarios());
     }
 
-    // Buscar usuário pelo e-mail
+
     @GetMapping("/email/{email}")
     public ResponseEntity<Usuario> buscarPorEmail(@PathVariable String email) {
         return usuarioService.buscarPorEmail(email)
@@ -46,14 +46,14 @@ public class UsuarioController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    // Atualizar usuário
+
     @PutMapping("/{id}")
     public ResponseEntity<Usuario> atualizarUsuario(@PathVariable Long id, @Valid @RequestBody Usuario usuarioAtualizado) {
         Usuario usuario = usuarioService.atualizarUsuario(id, usuarioAtualizado);
         return ResponseEntity.ok(usuario);
     }
 
-    // Deletar usuário
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarUsuario(@PathVariable Long id) {
         usuarioService.deletarUsuario(id);
